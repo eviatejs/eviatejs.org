b65e72661babbe4eba445b729ff8a6384c076e0f
apoorvcodes <tofu.develops@gmail.com>
2022-09-21 22:30:06 +0530

    feat: init

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+    return (
+        <div className="pt-16 pl-24 pr-24 flex justify-between items-end pb-6">
+        <div className="text-white text-2xl ">MIT License</div>
+        <div className="text-white text-2xl">@Eviatejs-core team</div>
+        </div>
+    )
+}
+
+export default Footer
